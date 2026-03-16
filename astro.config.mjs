5a2b876a81224460146d1efc27d01ead346522bd
G20-00 <sb5021087@gmail.com>
2026-03-15 20:52:29 -0500

    fix(i18n): cambiar a modo hybrid para soportar traducciones dinámicas

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://g20-00.github.io',
-  output: 'static',
+  output: 'hybrid',
 });
